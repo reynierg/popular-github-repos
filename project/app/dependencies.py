95cbebe927051ad1d86042c7d61281d917d041dd
Reynier Garcia <reynierg@protonmail.com>
2023-06-21 14:49:38 +0200

    refactor: move the repositories listing to a service class

--- project/app/dependencies.py
+++ project/app/dependencies.py
@@ -1,9 +1,10 @@
 from functools import lru_cache
 
-from fastapi import Request
+from fastapi import Depends, Request
 
 from app.config import Settings
 from app.github_client import GithubClient
+from app.repositories_service import RepositoriesService, RequestUrlMetadata
 
 
 @lru_cache
@@ -13,3 +14,12 @@ def get_settings() -> Settings:
 
 async def get_github_client(request: Request) -> GithubClient:
     return GithubClient(request.state.http_client)
+
+
+async def get_repositories_service(
+    request: Request, github_client: GithubClient = Depends(get_github_client)
+) -> RepositoriesService:
+    req_url_metadata = RequestUrlMetadata(
+        request.url.scheme, request.url.netloc, request.url.path
+    )
+    return RepositoriesService(github_client, req_url_metadata)
