80ba237edcb4fa1d170efef3b7fccf915bbf9d12
Reynier Garcia <reynierg@protonmail.com>
2023-06-21 02:07:13 +0200

    feat: add support for paginate over github repos

--- project/app/github_client.py
+++ project/app/github_client.py
@@ -1,3 +1,4 @@
+import typing as t
 from io import StringIO
 
 from httpx import AsyncClient
@@ -20,28 +21,35 @@ class GithubClient:
         return self._base_search_url
 
     async def get_repositories(
-        self, amount: str, from_date: str | None, language: str | None
-    ):
+        self,
+        per_page: str,
+        page: int | None,
+        from_date: str | None,
+        language: str | None,
+    ) -> t.Tuple[dict, dict]:
         """
-        Retrieves from the GitHub's API the amount of repositories specified.
-        According to the requirements, it is ONLY allowed, to specify to retrieve 10 OR 30 OR 50 repositories.
+        Retrieves from the GitHub's API a page of repositories, with per_page records.
+        According to the requirements, it is ONLY allowed, to specify to retrieve 10 OR 30 OR 50 repositories per page.
         """
         url_string_io: StringIO = StringIO()
         url_string_io.write(self._base_search_url)
 
-        required_amount = int(amount)
-        if required_amount > self._default_page_size:
-            # The API is being called to retrieve more than the 30 repositories, that are returned at most by default,
-            # from the GitHub's search API, with every call.
-            # This implies, that  30 < required_amount <= 100
-            required_amount = min(required_amount, self._max_allowed_page_size)
+        page_size = int(per_page)
+        if page_size > self._default_page_size:
+            # The API is being called to retrieve more than the 30 repositories per page,
+            # that are returned at most by default, from the GitHub's search API, with every call.
+            # Enforce that 30 < page_size <= 100:
+            page_size = min(page_size, self._max_allowed_page_size)
         else:
-            # 1 <= required_amount <= 30:
-            required_amount = max(1, required_amount)
+            # Enforce that:
+            # 1 <= page_size <= 30:
+            page_size = max(1, page_size)
 
         # Adjust properly the "per_page" query's string, to get at most, the amount of repositories requested,
         # in a single call, from the GitHub's search API:
-        url_string_io.write(f"&per_page={required_amount}")
+        url_string_io.write(f"&per_page={page_size}")
+        if page is not None:
+            url_string_io.write(f"&page={page}")
 
         if from_date and language:
             url_string_io.write(f"&q=created:>={from_date}+language:{language}")
@@ -56,4 +64,4 @@ class GithubClient:
         url: str = url_string_io.getvalue()
         response = await self._async_client.get(url)
         response.raise_for_status()
-        return response.json()
+        return response.json(), dict(response.headers)
--- project/app/routers/repositories.py
+++ project/app/routers/repositories.py
@@ -1,10 +1,11 @@
-import typing as ty
+import typing as t
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Request
 
 from app.dependencies import get_github_client
 from app.github_client import GithubClient
 from app.schemas import RepositorySummaryListSchema
+from app.utils import build_pagination_links
 
 router = APIRouter(prefix="/repositories", tags=["Repositories"])
 
@@ -15,15 +16,23 @@ router = APIRouter(prefix="/repositories", tags=["Repositories"])
     name="repositories:search-repositories",
 )
 async def search_repositories(
-    amount: ty.Annotated[
+    request: Request,
+    per_page: t.Annotated[
         str,
         Query(
-            title="Amount of GitHub repositories",
-            description="Amount of GitHub repositories to be returned",
+            title="Results per page",
+            description="Count of GitHub repositories to include per page",
             regex="^10|50|100$",
         ),
     ] = "10",
-    from_date: ty.Annotated[
+    page: t.Annotated[
+        int,
+        Query(
+            title="Page index",
+            description="Index of the page to fetch with repositories data",
+        ),
+    ] = None,
+    from_date: t.Annotated[
         str,
         Query(
             title="Lower bound date",
@@ -31,7 +40,7 @@ async def search_repositories(
             regex=r"^\d{4}-\d{2}-\d{2}$",
         ),
     ] = None,
-    language: ty.Annotated[
+    language: t.Annotated[
         str,
         Query(
             title="Language of the repositories",
@@ -40,4 +49,15 @@ async def search_repositories(
     ] = None,
     github_client: GithubClient = Depends(get_github_client),
 ) -> dict:
-    return await github_client.get_repositories(amount, from_date, language)
+    payload, headers = await github_client.get_repositories(
+        per_page, page, from_date, language
+    )
+    link_header: str | None = headers.get("link")
+    if link_header:
+        base_url = f"{request.url.scheme}://{request.url.netloc}{request.url.path}"
+        pagination = build_pagination_links(link_header, base_url)
+    else:
+        pagination = {"prev": None, "next": None}
+
+    payload.update({"pagination": pagination})
+    return payload
